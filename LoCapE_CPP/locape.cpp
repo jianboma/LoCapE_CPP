42b9d417404878bf572d7c309a1ca59c6c59e8a5
Jianbo Ma <jianboma5@gmail.com>
2019-09-12 13:23:45 +1000

    Added LoCapE.h and LoCapE.CPP.

---
+++ LoCapE_CPP/locape.cpp
@@ -0,0 +1,175 @@
+
+#include<locape.h>
+#include <omp.h>
+#define EIGEN_USE_MKL_ALL
+#include "unsupported/Eigen/MatrixFunctions"
+using namespace std;
+using namespace Eigen;
+
+void locape::set_arg(VectorXcd &data_a, double f1_a, double f2_a, int p_a, int M_a, int L_a, VectorXd eta_a)
+{
+    data = data_a;
+    f1 = f1_a; f2 = f2_a;
+    p = p_a; M = M_a; L = L_a;
+    eta = eta_a;
+
+    pn = int(floor(L/M)); // jump size
+    num_col = 2*p+1;
+    fv = Eigen::VectorXd::Zero(L);
+    indcol = Eigen::VectorXi::Zero(num_col); // define a complex vector
+
+    // assign fv
+    double bin = (double) 1.0/L;
+//    #pragma omp parallel for
+    for (int i=0;i<L;i++) {
+        fv(i) = 0+i*bin;
+    }
+    // assign indcol
+//    #pragma omp parallel for
+    for (int i=0;i<num_col;i++) {
+        indcol(i) = -p*pn+i*pn;
+    }
+
+    // assign frequency range and fourier matrix
+    jL = (int) floor(f1*L)+1;
+    jH = (int) ceil(f2*L)+1;
+    int mid;
+    if (jH+indcol(indcol.size()-1)>L)
+      mid = L-jL-indcol(0)+1;
+    else {
+        mid = jH+indcol(indcol.size()-1)-jL-indcol(0)+1;
+    }
+    indfreq = Eigen::VectorXi::Zero(mid);
+//    #pragma omp parallel for
+    for (int i=0;i<mid;i++) {
+            indfreq(i) = jL+indcol(0)+i-1;
+    } // assign indfreq
+    len_ind = (int) indfreq.size();
+    N = (int) data.size(); //number of samples
+    K = N-M+1; //number of smooth points
+
+    // assign original data
+    dataB = data.conjugate().colwise().reverse(); // conjugate and reverse slices
+    XF = Eigen::VectorXcd::Zero(M); // define a complex vector to store foreward data
+    XB = Eigen::VectorXcd::Zero(M); // define a complex vector to store backward data
+//    #pragma omp parallel for
+    for (int i=0;i<M;i++) {
+        XF(i) = data(i);
+        XB(i) = dataB(i);
+    } // assign first M elements to XF and XB
+
+    // initialize YB, YF and F (fourier matrix)
+    YF = MatrixXcd::Zero(len_ind, K);
+    YB = MatrixXcd::Zero(len_ind, K);
+    F = MatrixXcd::Zero(len_ind, M);
+//    #pragma omp parallel for
+    for (int i1=0;i1<len_ind;i1++) {
+        for (int i2=0;i2<M;i2++) {
+            F(i1,i2) = complex<double> (cos(-2*pi*fv(indfreq(i1))*i2),sin(-2*pi*fv(indfreq(i1))*i2));
+        }
+    }
+    // for output
+    XLoCapE = Eigen::MatrixXcd::Zero(eta.size(),jH-jL+1);
+    fL = Eigen::VectorXcd::Zero(jH-jL+1);
+//    #pragma omp parallel for
+    for (int i=0;i<jH-jL+1;i++) {
+        fL(i) = fv(i+jL);
+    }
+};
+
+
+
+void locape::transf_data()
+{
+    YF.col(0) = F*XF;
+    YB.col(0) = F*XB;
+    // assign other elements based on previous elements to reduce computation
+    Eigen::VectorXcd one_vec = Eigen::VectorXcd::Ones(len_ind);
+    Eigen::VectorXcd vec1, vec2,vec3, vec4, vec5, vec6, vec7; complex<double> point1, point2, point3, point4;
+//    #pragma omp parallel for
+    for (int i=1;i<K;i++) {
+        YF.col(i) = (YF.col(i-1)-data(i-1)*one_vec).cwiseProduct(F.col(1).conjugate())+data(M+i-1)*F.col(M-1);
+        point1 = data(i-1);point2 = data(M+i-1); vec1 = YF.col(i-1)-data(i-1)*one_vec;vec2 = F.col(1).conjugate();
+        vec3 = (YF.col(i-1)-data(i-1)*one_vec).cwiseProduct(F.col(1).conjugate()); vec4 = F.col(M-1);
+        vec5 = data(M+i-1)*F.col(M-1); vec6 = vec3+vec5; vec7 = point2*vec4; point3 = vec4(0); point4 = complex<float> (0.0019160000374540687,8.70000003487803e-05)*complex<float> (0.014179953373968601,0.9998994469642639);
+        YB.col(i) = (YB.col(i-1)-dataB(i-1)*one_vec).cwiseProduct(F.col(1).conjugate())+dataB(M+i-1)*F.col(M-1);
+    }
+}
+
+void locape::locape_calculate(const char* filename){
+
+    XLoCapEV = Eigen::VectorXcd::Zero(jH-jL+1);
+    maxIndex =  Eigen::VectorXi::Zero(jH-jL+1);
+    complex<double> mid_value = 0; // to record largest value in each damping factor iteration
+    int mid_ind = 0; // to record index of largest value in each damping factor iteration
+    // start with each frequency first, as the inverse of covariance can be used for each damping factor
+    MatrixXcd X1 = Eigen::MatrixXcd::Zero(indcol.size(),YF.cols());
+    MatrixXcd Xi1 = Eigen::MatrixXcd::Zero(indcol.size(),YF.cols());
+    MatrixXcd Fsub = Eigen::MatrixXcd::Zero(indcol.size(),M);
+
+//    #pragma omp parallel for
+    for (int jl=jL;jl<jH;jl++) {
+        // pick columns from transformed data
+        for (int i1=0;i1<indcol.size();i1++) {
+            X1.row(i1) = YF.row(jl+indcol(i1)-indfreq(0)-1);
+            Xi1.row(i1) = YB.row(jl+indcol(i1)-indfreq(0)-1);
+            Fsub.row(i1) = F.row(jl+indcol(i1)-indfreq(0)-1);
+        }
+        // calculate covariance matrix and its inverse
+        MatrixXcd mat1 = X1.transpose().conjugate();
+        MatrixXcd R = (X1*X1.transpose().conjugate()+Xi1*Xi1.transpose().conjugate())/2.0;
+        MatrixXcd Rinv = R.inverse();
+        mid_value = 0; // initialize
+        mid_ind = 0; // initialize
+        #pragma omp parallel for
+        for (int neta=0;neta<eta.size();neta++) {
+            // calculate scalars
+            double Le = (1-exp(-2*eta(neta)*K))/(1-exp(-2*eta(neta)));
+            double LeN = (1-exp(-2*eta(neta)*N))/(1-exp(-2*eta(neta)));
+            if (eta(neta) == 0)
+            {
+                Le = K;
+                LeN = N;
+            }
+            // calculate fourier vectors
+            Eigen::VectorXcd a_vec1 = Eigen::VectorXcd::Zero(M);
+            double pointa1, pointa2, pointa3, pointa4, pointa5, pointa6, pointa7;
+            complex<double> pointa8;
+            #pragma omp parallel for
+            for (int i=0;i<M;i++) {
+                a_vec1(i) = complex<double> (exp(-1*eta(neta)*i)*cos((2*pi*fv(jl-1))*i),exp(-1*eta(neta)*i)*sin((2*pi*fv(jl-1))*i));
+                pointa1 = -1*eta(neta); pointa2 = fv(jl-1);
+                pointa3 = exp(-1*eta(neta)*i); pointa4=cos((2*pi*fv(jl-1))*i);pointa5=sin((2*pi*fv(jl-1))*i);
+                pointa6 = exp(-1*eta(neta)*i)*cos((2*pi*fv(jl-1))*i); pointa7 = exp(-1*eta(neta)*i)*sin((2*pi*fv(jl-1))*i);
+                pointa8 = complex<double> (exp(-1*eta(neta)*i)*cos((2*pi*fv(jl-1))*i),exp(-1*eta(neta)*i)*sin((2*pi*fv(jl-1))*i));
+            }
+            Eigen::VectorXcd a_vec = Fsub*a_vec1;
+            VectorXcd vec1, vec2; complex<double> point1;
+            vec1 = Fsub.row(0).head(M);
+            vec2 = a_vec1.head(M);
+            point1 = (vec1.transpose()*vec2).value();
+
+            // calculate least square vectors
+            Eigen::VectorXcd s_vec = Eigen::VectorXcd::Zero(K);
+            #pragma omp parallel for
+            for (int i=0;i<K;i++) {
+                s_vec(i) = complex<double> (1.0/Le*exp(-1*eta(neta)*i)*cos((-2*pi*fv(jl-1))*i),1.0/Le*exp(-1*eta(neta)*i)*sin((-2*pi*fv(jl-1))*i));
+            }
+
+            // calculate the amplitude
+            VectorXcd vec10 = (a_vec.conjugate().transpose()*Rinv*X1*s_vec);
+            VectorXcd vec11 = ((a_vec.conjugate().transpose()*Rinv)*a_vec);
+            complex<double> point10 = vec10(0)/vec11(0);
+            Eigen::VectorXcd mag = (a_vec.conjugate().transpose()*Rinv*X1*s_vec)/((a_vec.conjugate().transpose()*Rinv)*a_vec);
+            XLoCapE(neta,jl-jL) = mag(0);
+            if (abs(mag(0))> abs(mid_value)){
+                mid_value = mag(0);
+                mid_ind = neta;
+            }
+        }
+        Eigen::VectorXcd vec1 = XLoCapE.col(jl-jL);
+        XLoCapEV(jl-jL) = mid_value;
+        maxIndex(jl-jL) = mid_ind;
+    }
+}
+
---
+++ LoCapE_CPP/locape.h
@@ -0,0 +1,78 @@
+#ifndef LOCAPE_H
+#define LOCAPE_H
+
+#endif // LOCAPE_H
+
+#define EIGEN_USE_MKL_ALL
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include "unsupported/Eigen/FFT"
+#include "unsupported/Eigen/MatrixFunctions"
+#include "Eigen/Dense"
+#include "Eigen/Core"
+#include <iomanip>
+#include <omp.h>
+
+using namespace std;
+using namespace Eigen;
+
+class locape{
+
+
+public:
+    VectorXcd data;
+    double f1; double f2;
+    int p; int M; int L;
+    VectorXd eta;
+    Eigen::MatrixXcd XLoCapE; // define a matrix to record locape estimate
+    Eigen::VectorXcd XLoCapEV; // define a vector to record locape estimate
+    VectorXcd fL; // the frequencies analysed
+    VectorXi maxIndex;
+
+    double pi = 3.141592653589793;
+    int pn; // jump size
+    int N; // data size
+    int num_col;
+    int jL; int jH;
+    int len_ind;
+    int K; // number of smooth points
+
+    Eigen::VectorXd fv;
+    Eigen::VectorXi indcol; // define a complex vector
+    Eigen::VectorXi indfreq; // define a complex vector
+    Eigen::VectorXcd dataB; // define a complex vector to store backward data
+    Eigen::VectorXcd XF; // define a complex vector to store foreward data
+    Eigen::VectorXcd XB; // define a complex vector to store backward data
+    Eigen::MatrixXcd YF; // define a complex matrix to store transformed foreward data
+    Eigen::MatrixXcd YB; // define a complex matrix to store transformed backward data
+    Eigen::MatrixXcd F; // define a fourier matrix
+
+    void set_arg(VectorXcd &data_a, double f1_a, double f2_a, int p_a, int M_a, int L_a, VectorXd eta_a);
+    void transf_data(); // transfer the original time-sequence data into frequency domain through predefined fourier matrix F
+    void locape_calculate(const char* filename); // calculate
+
+private:
+//    float pi = 3.1415926;
+//    int pn; // jump size
+//    int N; // data size
+//    int num_col;
+//    int jL; int jH;
+//    int len_ind;
+//    int K; // number of smooth points
+
+//    Eigen::VectorXf fv;
+//    Eigen::VectorXi indcol; // define a complex vector
+//    Eigen::VectorXi indfreq; // define a complex vector
+//    Eigen::VectorXcf dataB; // define a complex vector to store backward data
+//    Eigen::VectorXcf XF; // define a complex vector to store foreward data
+//    Eigen::VectorXcf XB; // define a complex vector to store backward data
+//    Eigen::MatrixXcf YF; // define a complex matrix to store transformed foreward data
+//    Eigen::MatrixXcf YB; // define a complex matrix to store transformed backward data
+//    Eigen::MatrixXcf F; // define a fourier matrix
+
+
+
+
+
+};
